f2035450b39ff16ccb26668d7c189e0b810da85d
Fred Feraco <frederick.feraco@gmail.com>
2023-08-23 13:44:17 -0400

    Update web_search.py

--- actions/web_search.py
+++ actions/web_search.py
@@ -1,25 +1,42 @@
 from __future__ import annotations
 import json
-from duckduckgo_search import DDGS
+import requests
 
-ddgs = DDGS()
+def search_arxiv(query: str, num_results: int = 4) -> str:
+    base_url = "http://export.arxiv.org/api/query?"
+    response = requests.get(base_url, params={"search_query": query, "max_results": num_results})
+    # Parse the response as needed
+    return response.text
 
-def web_search(query: str, num_results: int = 4) -> str:
-    """Useful for general internet search queries."""
-    print("Searching with query {0}...".format(query))
-    search_results = []
-    if not query:
-        return json.dumps(search_results)
+def search_crossref(query: str, num_results: int = 4) -> str:
+    base_url = "https://api.crossref.org/works?"
+    response = requests.get(base_url, params={"query": query, "rows": num_results})
+    return response.json()
 
-    results = ddgs.text(query)
-    if not results:
-        return json.dumps(search_results)
+def search_ntrs(query: str, num_results: int = 4) -> str:
+    # Assuming NTRS provides an API endpoint (this is a placeholder)
+    base_url = "https://ntrs.nasa.gov/api/search?"
+    response = requests.get(base_url, params={"q": query, "rows": num_results})
+    return response.json()
 
-    total_added = 0
-    for j in results:
-        search_results.append(j)
-        total_added += 1
-        if total_added >= num_results:
-            break
+def search_openalex(query: str, num_results: int = 4) -> str:
+    base_url = "https://api.openalex.org/search?"
+    response = requests.get(base_url, params={"q": query, "rows": num_results})
+    return response.json()
 
-    return json.dumps(search_results, ensure_ascii=False, indent=4)
+def web_search(query: str, database: str, num_results: int = 4) -> str:
+    print(f"Searching {database} with query {query}...")
+    if database == "arxiv":
+        return search_arxiv(query, num_results)
+    elif database == "crossref":
+        return search_crossref(query, num_results)
+    elif database == "ntrs":
+        return search_ntrs(query, num_results)
+    elif database == "openalex":
+        return search_openalex(query, num_results)
+    else:
+        raise ValueError(f"Unknown database: {database}")
+
+# Example usage
+results = web_search("space exploration", "arxiv", 4)
+print(results)
